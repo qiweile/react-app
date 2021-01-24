2f977aa15f003309b46989554879d5c44fe15050
sunbaoning <sunbaoning8@126.com>
2021-01-24 18:39:14 +0800

    引入并简单使用路由

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Provider } from 'react-redux' // Provider标签内的所有组件都可以使用该组件上的数据
+import {BrowserRouter,Route} from 'react-router-dom'
 import store from './store'
 import { Globalstyle } from './style'
 import Header from './common/header'
@@ -7,6 +8,10 @@ function App() {
         <Provider store={store}>
             <Globalstyle />
             <Header />
+            <BrowserRouter>
+                <Route path="/" exact render={() => <div>首页</div>}></Route>
+                <Route path="/detail" exact render={() => <div>详情页</div>}></Route>
+            </BrowserRouter>
         </Provider>
     );
 }

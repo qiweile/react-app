bb850d0a43d0ec3b5a21569fc8343534b75aceb6
sunbaoning <sunbaoning8@126.com>
2021-01-24 15:37:02 +0800

    redux和react-redux的用法

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Provider } from 'react-redux' // Provider标签内的所有组件都可以使用该组件上的数据
+import store from './store'
 import { Globalstyle } from './style'
 import Header from './common/header'
 function App() {
     return (
-        <div>
+        <Provider store={store}>
             <Globalstyle />
             <Header />
-        </div>
+        </Provider>
     );
 }
 export default App;
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,9 +1,38 @@
 import { Title, Log } from './style'
-export default () => {
+import {connect} from 'react-redux'
+const Head = props => {
+    console.log(props)
     return (
         <Title>
-            你好啊！
+            {props.hello}
+            {props.focused + ''}
             {/* Log 可以直接看做 a 标签使用 */}
+            <button onClick={props.submitBtn}>改为true</button>
+            <button onClick={props.submitBtn1}>改为false</button>
             <Log href='/' />
-        </Title>)
+        </Title>
+    )
 }
+const mapStateToProps = (state) => {
+    return {
+        focused: state.focused,
+        hello: state.hello
+    }
+}
+const mapDispathToProps = (dispatch) => {
+    return {
+        submitBtn () {
+            const action = {
+                type: 'search_focus',
+            }
+            dispatch(action)
+        },
+        submitBtn1 () {
+            const action = {
+                type: 'search_focus1'
+            }
+            dispatch(action)
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispathToProps)(Head)
